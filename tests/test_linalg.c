eec82c95313bb94de0e9e09672832931aa5d68b6
ulvdamme <ulysse.vandamme@ugent.be>
2026-04-11 19:53:17 +0200

    tests for poweriteration

--- tests/test_linalg.c
+++ tests/test_linalg.c
@@ -11,8 +11,6 @@
 
 
 void test_QR_factorization() {
-    // TODO 
-
     // 1 -1.0 1
     // 1 -0.5 0.25
     // 1 0.0  0.0
@@ -38,13 +36,26 @@ void test_QR_factorization() {
     set_value(A, 3, 2, 0.25);
     set_value(A, 4, 2, 1);
 
-    print_matrix(A);
     QR_result result = QR_decomposition(A);
     free_matrix(A);
 
 }
 
+test_poweriteration() {
+    Matrix* A = make_empty_matrix(2, 2);
+
+    set_value(A, 0, 0, 1.5);
+    set_value(A, 1, 0, 0.5);
+    set_value(A, 0, 1, 0.5);
+    set_value(A, 1, 1, 1.5);
+
+    float result = power_iteration(A, 9);
+
+    ASSERT_EQ(result, 2.0);
+}
+
 int main() {
     test_QR_factorization();
+    test_poweriteration();
     return 0;
 }
